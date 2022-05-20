4f6b1d3620bef68dae2a758032ff577c67b431a4
Chris Granum <chris.granum@gmail.com>
2022-05-20 08:59:07 -0400

    split config.ini into a docked and undocked config removing the backlight module from docked. Updated launch.sh to determine whether primary monitor is active and select appropriate dock.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,10 +1,34 @@
 #!/usr/bin/env bash
 
+# define laptop monitor
+laptop_monitor="eDP-1"
+
+# define bars
+docked_bar="config_docked.ini"
+undocked_bar="config_undocked.ini"
+
+# define colors for printf commands
+text_color='\033[0;32m'
+no_color='\033[0m'
+
 # Terminate already running bar instances
 killall polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch polybar
-polybar main -c $(dirname $0)/config.ini &
+# Determine whether laptop monitor is active
+case "$(xrandr --listactivemonitors | grep -c $laptop_monitor)" in
+    0)
+        # laptop monitor is not active, launch docked bar
+        printf "${text_color}launch.sh ${no_color} laptop monitor is not active, launching docked bar\n"
+        polybar main -c $(dirname $0)/$docked_bar &
+        ;;
+    *)
+        # laptop monitor is active, launch undocked bar
+        printf "${text_color}launch.sh ${no_color} laptop monitor is  active, launching undocked bar\n"
+        polybar main -c $(dirname $0)/$undocked_bar &
+        ;;
+esac
+
+
